aa9071fba9a3fdaad0d23b0780e195c975234266
Anthony <txmack713@mack-daddys-hp.home>
2021-07-22 06:27:08 -0500

    Completed the MDN JavaScript Function Return Values section

---
+++ r-values.js
@@ -0,0 +1,58 @@
+const btn = document.querySelector('button');
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+let WIDTH = document.documentElement.clientWidth;
+let HEIGHT = document.documentElement.clientHeight;
+
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
+
+function random(number) {
+    return Math.floor(Math.random()*number);
+}
+
+function draw() {
+    ctx.clearRect(0,0,WIDTH,HEIGHT);
+    for(let i = 0; i < 100; i++) {
+        ctx.beginPath();
+        ctx.fillStyle = 'rgba(255,0,0,0.5)';
+        ctx.arc(random(WIDTH), random(HEIGHT), random(50), 0, 2 * Math.PI);
+        ctx.fill();
+    }
+}
+
+btn.addEventListener('click',draw);
+
+// Function Library Active Learning
+const input = document.querySelector('.numberInput');
+const para = document.querySelector('p');
+
+function squared(num) {
+    return num*num;
+}
+
+function cubed(num) {
+    return num*num*num;
+}
+
+function factorial(num) {
+    if (num < 0) return undefined;
+    if (num == 0) return 1;
+    let x = num - 1;
+    while (x > 1) {
+        num *= x;
+        x--;
+    }
+    return num;
+}
+
+input.onchange = function() {
+    const num = parseFloat(input.value);
+    if (isNan(num)) {
+        para.textContent = 'You need to enter a number!';
+    } else {
+        para.textContent = num + ' squared is ' + squared(num) + '. ' +
+            num + ' cubed is ' + cubed(num) + '. ' + num + ' factorial is ' + factorial(num) + '.';
+    }
+}
