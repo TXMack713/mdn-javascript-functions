f83219691d67d4c7671cb8864f2a2fc150e332a3
TXMack713 <mackattack713@gmail.com>
2021-07-22 07:56:57 -0500

    2 files modified, 1 added

--- assessment.js
+++ assessment.js
@@ -1,3 +1,5 @@
+// MDN JavaScript Functions Assessment 1
+
 let names = [
   'Chris',
   'Li Kang',
@@ -16,6 +18,23 @@ para.textContent = names[Math.floor(Math.random() * names.length)];
 
 // Don't edit the code below here!
 
-const section = document.querySelector('.assessment');
+const section = document.querySelector('.assessment1');
 
 section.appendChild(para);
+
+// MDN JavaScript Functions Assessment 2
+
+let canvas = document.querySelector('canvas');
+let ctx = canvas.getContext('2d');
+
+let x = 50;
+let y = 60;
+let width = 100;
+let height = 75;
+let color = 'blue';
+
+// Add your code here
+
+// Don't edit the code below here!
+
+const section2 = document.querySelector('.preview');
